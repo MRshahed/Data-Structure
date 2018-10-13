275fc8876c74edc1ce5bb85b548ecda95a4e0409
Shahed hossen <shahedhossensaad@gmail.com>
2018-10-13 23:29:38 +0600

    Update Array-1D.cpp

--- Array-1D.cpp
+++ Array-1D.cpp
@@ -5,12 +5,34 @@ int main()
 ios_base::sync_with_stdio(false);
 cin.tie(0);
 
-  int n;
+	int n;
 	cin>>n;
 	int a[n];
-  for(int i=0; i<n; i++)
+
+	for(int i=0; i<n; i++)
 	{
 		cin>>a[i];
-		cout<<a[i]<<endl;
+		cout<<a[i]<<" ";               /*Printing Array element*/
 	}
+	cout<<endl;
+	for(int i=n-1; i>=0; i--)
+	{
+		cout<<a[i]<<" ";               /*Printing Array element in reverse order*/
+	}
+	cout<<endl;
+	
+	int sum=0;
+	for(int i=0; i<n; i++)
+	{
+		sum+=a[i];             
+	}
+	cout<<sum<<endl;                   /*Printing sum of the elements of Array*/
+	
+	int multi=1;
+	for(int i=0; i<n; i++)
+	{
+		multi*=a[i];
+	}
+	cout<<multi<<endl;                 /*Printing multiplication of the elements of Array*/
+		
 }
